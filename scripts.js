725c1bda79e301a4deff671a936b9ba1ac5851d6
Sasha <kupchenko555@gmail.com>
2023-08-29 09:51:46 +0300

    Goods, Card, Adaptive, Cart

--- scripts.js
+++ scripts.js
@@ -25,7 +25,7 @@ const nextButton = document.querySelector('.next-button');
 const currentPageSpan = document.getElementById('current-page');
 const totalPagesSpan = document.getElementById('total-pages');
 
-let currentPhotoIndex = 0;
+let currentPhotoIndex = 1;
 
 function showPhoto(index) {
     galleryPhotos.forEach((photo, idx) => {
@@ -50,5 +50,91 @@ nextButton.addEventListener('click', () => {
     showPhoto(currentPhotoIndex);
 });
 
-totalPagesSpan.textContent = galleryPhotos.length.toString().padStart(2, '0'); 
-showPhoto(currentPhotoIndex);
+
+// counter
+
+// console.log('Mountain Travel');
+
+// const btnMinus = document.querySelector('[data-action="minus"]');
+// const btnPlus = document.querySelector('[data-action="plus"]');
+// const counter = document.querySelector('[data-counter]');
+
+// btnMinus.addEventListener('click', function(){
+//     if (parseInt(counter.innerText) > 1){
+//     counter.innerText = --counter.innerText;
+//     }
+// });
+
+// btnPlus.addEventListener('click', function(){
+//     counter.innerText = ++counter.innerText;
+// });
+
+window.addEventListener('click', function (event) {
+
+    let counter;
+    if (event.target.dataset.action === 'plus' || event.target.dataset.action === 'minus'){
+      const counterWrapper = event.target.closest('.counterblock');
+      counter = counterWrapper.querySelector('[data-counter]');
+    }
+    if (event.target.dataset.action === 'plus'){
+      counter.innerText = ++counter.innerText;
+    }
+    if (event.target.dataset.action === 'minus'){
+      if (parseInt(counter.innerText) > 1){
+            counter.innerText = --counter.innerText;
+            }
+            else if (event.target.closest('.product-buy') && parseInt(counter.innerText) == 1){
+                event.target.closest('.product-item').remove();
+            }
+    }
+});
+const productWrapper = document.querySelector('.product-buy');
+window.addEventListener('click', function (event) {
+    if (event.target.hasAttribute('data-cart')){
+        const card = event.target.closest('.item');
+        const productInfo = {
+            id: card.dataset.id,
+            imgSrc: card.querySelector('.product-img').getAttribute('src'),
+            title: card.querySelector('.item-title').innerText,
+            price: card.querySelector('.price-p').innerText,
+            counter: card.querySelector('[data-counter]').innerText,
+        };
+
+
+        const itemInProduct = productWrapper.querySelector(`[data-id="${productInfo.id}"]`);
+        if (itemInProduct){
+           const counterElement = itemInProduct.querySelector('[data-counter]');
+           counterElement.innerText = parseInt(counterElement.innerText) + parseInt(productInfo.counter);
+        } else{
+
+        
+
+        const productItemHtml = `<div class="product-buy">
+        <div class="product-item" data-id="${productInfo.id}">
+        <div class="product-item-top">
+            <div class="product-image">
+                <img src="${productInfo.imgSrc}" alt="${productInfo.title}">
+            </div>
+            <div class="product-item-desc">
+                <div class="product-item-title">${productInfo.title}</div>
+                <div class="item-small counterblock">
+                    <button class="item-control" data-action="minus">-</button>
+                    <div class="current" data-counter>${productInfo.counter}</div>
+                    <button class="item-control" data-action="plus">+</button>
+                </div>
+                <div class="price">
+                    <div class="price-p">${productInfo.price}</div>
+                </div>
+
+    </div>
+        </div>
+            </div>
+
+    </div>`;
+        productWrapper.insertAdjacentHTML('beforeend', productItemHtml)
+        }
+
+        card.querySelector('[data-counter]').innerText ='1';
+    }
+});
+
